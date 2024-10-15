eba9543bd75269c7a58337748a2fadc213d6f4df
Denis Lopez <denuxs@gmail.com>
2024-10-15 15:53:18 -0600

    debug render build

--- render-build.sh
+++ render-build.sh
@@ -6,11 +6,14 @@ STORAGE_DIR=/opt/render/project
 mkdir -p $STORAGE_DIR/chrome
 cd $STORAGE_DIR/chrome
 
+echo $HOME
+pwd
+
 echo "...Downloading Chrome"
 wget -P ./ https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 dpkg -x ./google-chrome-stable_current_amd64.deb $STORAGE_DIR/chrome
 rm ./google-chrome-stable_current_amd64.deb
-cd ..
+cd $HOME/project
 
 # add your own build commands...
 pip install -r requirements.txt
