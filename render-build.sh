52602cdcb63964424571d9172180446c0666c773
Denis Lopez <denuxs@gmail.com>
2024-10-15 15:08:46 -0600

    update chrome install

--- render-build.sh
+++ render-build.sh
@@ -2,10 +2,15 @@
 # exit on error
 set -o errexit
 
+STORAGE_DIR=/opt/render/project
+mkdir -p $STORAGE_DIR/chrome
+cd $STORAGE_DIR/chrome
+
 echo "...Downloading Chrome"
 wget -P ./ https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 dpkg -x ./google-chrome-stable_current_amd64.deb $STORAGE_DIR/chrome
 rm ./google-chrome-stable_current_amd64.deb
+cd ..
 
 # add your own build commands...
 pip install -r requirements.txt
